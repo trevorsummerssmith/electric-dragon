56b79b030117ce369301e5951bf1681b2ad57d0c
Trevor Summers Smith <trevorsummerssmith@gmail.com>
2011-09-30 09:07:54 -0400

    Add type-specific getters for Cell's PlacedObject

--- src/pkg/game/cell.go
+++ src/pkg/game/cell.go
@@ -21,6 +21,9 @@ type Cell interface {
 	AddPlacedObject(p PlacedObject)
 	RemovePlacedObject(p PlacedObject)
 
+	Characters() []Character
+	NonCharacterObjects() []PlacedObject
+
 	Idx() Idx
 	SetIdx(idx Idx)
 	CellType() CellType
@@ -58,6 +61,30 @@ func (c *BasicCell) RemovePlacedObject(p PlacedObject) {
 			return
 		}
 	}
+}
+
+func (c *BasicCell) Characters() []Character {
+	cs := make([]Character, 0)
+	for i := 0; i < len(c.placedObjects); i++ {
+		switch guy := c.placedObjects[i].(type) {
+		case Character:
+			cs = append(cs, guy)
+		}
+	}
+	return cs
+}
+
+func (c *BasicCell) NonCharacterObjects() []PlacedObject {
+	ps := make([]PlacedObject, 0)
+	for i := 0; i < len(c.placedObjects); i++ {
+		switch guy := c.placedObjects[i].(type) {
+		case Character:
+			continue
+		default:
+			ps = append(ps, guy)
+		}
+	}
+	return ps
 }
 
 func (c *BasicCell) Idx() Idx {
